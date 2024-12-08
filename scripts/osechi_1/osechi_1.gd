e9d8d0b94ccbbaaa6227918dab935cc65459f9b4
i-kk65536 <lockegh512@gmail.com>
2024-12-08 14:56:16 +0900

    置くことができるか判定する

--- scripts/osechi_1/osechi_1.gd
+++ scripts/osechi_1/osechi_1.gd
@@ -1,5 +1,6 @@
 extends Area2D
 
+signal placed_osechi_1
 var _drag_mode = false
 
 func _ready() -> void:
@@ -14,17 +15,15 @@ func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 			_drag_mode = true
-		else:
+		elif event.button_index == MOUSE_BUTTON_LEFT and not event.pressed:
 			_drag_mode = false
-
-	elif event is InputEventMouseMotion:
-		print("Mouse Motion at: ", event.position)
+			emit_signal("placed_osechi_1")
 
 func snap_to_grid(axis: String):
 	var mouse_pos = get_viewport().get_mouse_position()[axis]
-	if fmod(position[axis], Grobal.osechi_size) < (Grobal.osechi_size / 4):
-		position[axis] = (int(mouse_pos) / Grobal.osechi_size) * Grobal.osechi_size
-	elif fmod(position[axis], Grobal.osechi_size) >= (Grobal.osechi_size * 3 / 4):
-		position[axis] = ((int(mouse_pos) / Grobal.osechi_size) + 1) * Grobal.osechi_size
+	if fmod(position[axis], Global.osechi_size) < (Global.osechi_size / 4):
+		position[axis] = (int(mouse_pos) / Global.osechi_size) * Global.osechi_size
+	elif fmod(position[axis], Global.osechi_size) >= (Global.osechi_size * 3 / 4):
+		position[axis] = ((int(mouse_pos) / Global.osechi_size) + 1) * Global.osechi_size
 	else:
 		position[axis] = mouse_pos

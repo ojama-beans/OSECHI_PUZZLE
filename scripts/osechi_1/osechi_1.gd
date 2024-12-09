7a616336d7a7ed739a460916dbe3f808c5c6151b
i-kk65536 <lockegh512@gmail.com>
2024-12-09 19:38:42 +0900

    スナップの基準点を変更

--- scripts/osechi_1/osechi_1.gd
+++ scripts/osechi_1/osechi_1.gd
@@ -16,52 +16,49 @@ func _process(delta: float) -> void:
 	if _drag_mode:
 		position = get_viewport().get_mouse_position() - _diff
 	elif _on_grid:
-		if overlap():
-			move_towards(_prev_pos, delta * _return_speed)
-		else:
-			snap_to_grid()
-			place_on_grid()
-			placed_osechi_1.emit()
-			set_process_input(false)
-
+		# if overlap():
+			# move_towards(_prev_pos, delta * _return_speed)
+		# else:
+		snap_to_grid()
+		place_on_grid()
+		placed_osechi_1.emit()
+		set_process(false)
+		set_process_input(false)
+		
 func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
-	if event is InputEventMouseButton:
-		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-			if not _drag_mode:
-				_prev_pos = position
-				_diff = get_viewport().get_mouse_position() - position
+	if event is not InputEventMouseButton:
+		return
+	if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+		if not _drag_mode:
+			_prev_pos = position
+			_diff = get_viewport().get_mouse_position() - position
 			_drag_mode = true
-		elif event.button_index == MOUSE_BUTTON_LEFT and not event.pressed:
-			_drag_mode = false
-			if on_grid():
-				_on_grid = true
+	elif event.button_index == MOUSE_BUTTON_LEFT and not event.pressed:
+		_drag_mode = false
+		if on_grid():
+			_on_grid = true
 
 func snap_to_grid() -> void:
-	snap_to_grid_axis("x")
-	snap_to_grid_axis("y")
-
-func snap_to_grid_axis(axis: String) -> void:
-	var mouse_pos = get_viewport().get_mouse_position()[axis]
-	var pos = position[axis] - Global.origin[axis]
-	var modded_pos = fmod(pos, Global.osechi_size)
-	var ratio_on_grid
-	if modded_pos < (Global.osechi_size / 2):
-		ratio_on_grid = int(mouse_pos - Global.origin[axis]) / Global.osechi_size
-	else:
-		ratio_on_grid = (int(mouse_pos - Global.origin[axis]) / Global.osechi_size) + 1
-	position[axis] = ratio_on_grid * Global.osechi_size + Global.origin[axis]
+	for axis in ["x", "y"]:
+		var mouse_pos = get_viewport().get_mouse_position()[axis] - _diff[axis]
+		var pos = position[axis] - Global.origin[axis]
+		var modded_pos = fmod(pos, Global.osechi_size)
+		var ratio_on_grid
+		if modded_pos < (Global.osechi_size / 2):
+			ratio_on_grid = int(mouse_pos - Global.origin[axis]) / Global.osechi_size
+		else:
+			ratio_on_grid = (int(mouse_pos - Global.origin[axis]) / Global.osechi_size) + 1
+		position[axis] = ratio_on_grid * Global.osechi_size + Global.origin[axis]
 
 func on_grid() -> bool:
-	var pos_on_grid = Vector2(position.x - Global.origin.x, position.y - Global.origin.y)
+	var pos_on_grid = position - Vector2(Global.origin)
 	var grid_end = Global.grid_size - _shape[0].size()
 	var out_length = Global.osechi_size / 3
 	return ((-out_length <= pos_on_grid.x and pos_on_grid.x <= grid_end + out_length) and
 			(-out_length <= pos_on_grid.y and pos_on_grid.y <= grid_end + out_length))
 
 func place_on_grid() -> void:
-	var pos_on_grid = Vector2i(position.x - Global.origin.x, position.y - Global.origin.y)
-	pos_on_grid = Vector2i(pos_on_grid.x / Global.osechi_size, pos_on_grid.y / Global.osechi_size)
+	var pos_on_grid_modded = (Vector2i(position) - Global.origin) / Global.osechi_size
 	for i in _shape[0].size():
 		for j in _shape.size():
-			Global.grid[pos_on_grid.y + i][pos_on_grid.x + j] = _shape[i][j]
-	print(Global.grid)
+			Global.grid[pos_on_grid_modded.y + i][pos_on_grid_modded.x + j] = _shape[i][j]

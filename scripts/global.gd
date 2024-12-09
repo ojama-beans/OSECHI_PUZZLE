f0676f03117317ae545d33957aab96eb72d33d88
i-kk65536 <lockegh512@gmail.com>
2024-12-09 19:00:30 +0900

    コードの簡略化

--- scripts/global.gd
+++ scripts/global.gd
@@ -14,11 +14,10 @@ var grid = [
 ]
 const osechi_shape = {
 	"Osechi_1": [
-		[1, 1], 
-		[1, 1]
+		[2**0, 2**0], 
+		[2**0, 2**0]
 	],
 }
 const screen_size = Vector2i(720, 1280)
-# grid_size is constant
-var grid_size = osechi_size * (grid[0].size() - 2)
+var grid_size = osechi_size * grid.size()
 var origin = Vector2i((screen_size.x - grid_size) / 2, (screen_size.y - grid_size) / 2)
--- scripts/osechi_1/osechi_1.gd
+++ scripts/osechi_1/osechi_1.gd
@@ -4,6 +4,7 @@ signal placed_osechi_1
 
 var _has_moved = false
 var _drag_mode = false
+const _shape = Global.osechi_shape["Osechi_1"]
 
 func _ready() -> void:
 	pass
@@ -34,20 +35,24 @@ func snap_to_grid_axis(axis: String) -> void:
 	var mouse_pos = get_viewport().get_mouse_position()[axis]
 	var pos = position[axis] - Global.origin[axis]
 	var modded_pos = fmod(pos, Global.osechi_size)
+	var ratio_on_grid
 	if modded_pos < (Global.osechi_size / 2):
-		position[axis] = (int(mouse_pos - Global.origin[axis]) / Global.osechi_size) * Global.osechi_size + Global.origin[axis]
+		ratio_on_grid = int(mouse_pos - Global.origin[axis]) / Global.osechi_size
 	else:
-		position[axis] = ((int(mouse_pos - Global.origin[axis]) / Global.osechi_size) + 1) * Global.osechi_size + Global.origin[axis]
+		ratio_on_grid = (int(mouse_pos - Global.origin[axis]) / Global.osechi_size) + 1
+	position[axis] = ratio_on_grid * Global.osechi_size + Global.origin[axis]
 
 func on_grid() -> bool:
 	var pos_on_grid = Vector2(position.x - Global.origin.x, position.y - Global.origin.y)
+	var grid_end = Global.grid_size - _shape[0].size()
 	var out_length = Global.osechi_size / 3
-	return (-out_length <= pos_on_grid.x and pos_on_grid.x <= Global.grid_size + out_length) and (-out_length <= pos_on_grid.y and pos_on_grid.y <= Global.grid_size + out_length)
+	return ((-out_length <= pos_on_grid.x and pos_on_grid.x <= grid_end + out_length) and
+			(-out_length <= pos_on_grid.y and pos_on_grid.y <= grid_end + out_length))
 
 func place_on_grid() -> void:
 	var pos_on_grid = Vector2i(position.x - Global.origin.x, position.y - Global.origin.y)
 	pos_on_grid = Vector2i(pos_on_grid.x / Global.osechi_size, pos_on_grid.y / Global.osechi_size)
-	const shape = Global.osechi_shape["Osechi_1"]
-	for i in shape[0].size():
-		for j in shape.size():
-			Global.grid[pos_on_grid.y + i][pos_on_grid.x + j] = shape[i][j]
+	for i in _shape[0].size():
+		for j in _shape.size():
+			Global.grid[pos_on_grid.y + i][pos_on_grid.x + j] = _shape[i][j]
+	print(Global.grid)

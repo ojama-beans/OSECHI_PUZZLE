e3db38455f03ebb0c439e40b3c3ff3dbafd7586c
i-kk65536 <lockegh512@gmail.com>
2024-12-13 07:04:36 +0900

    演出を場合分け

--- scripts/combo/main.gd
+++ scripts/combo/main.gd
@@ -14,15 +14,19 @@ func _process(delta: float) -> void:
 	pass
 
 func _on_combo_occurred(combo: int) -> void:
-	canvas_layer_node.visible = true
-	Score.combo()
-	combo_effct(combo)
+	if Global.combo_map.get(combo, "None") != "None":
+		canvas_layer_node.visible = true
+		Score.combo()
+		combo_effct(combo)
 
 func _on_video_finished() -> void:
 	canvas_layer_node.visible = false
 	SignalManager.combo_ended.emit()
 
 func combo_effct(combo: int) -> void:
-	if Global.combo_map.get(combo, "None") == "sison_hanei":
-		video_stream_player_node.finished.connect(_on_video_finished)
-		video_stream_player_node.play()
+	if Global.combo_map[combo] == "sison_hanei":
+		var video_stream_player_node = $VideoStreamPlayer
+	if Global.combo_map[combo] == "kenko_tyoju":
+		var video_stream_player_node = $VideoStreamPlayer
+	video_stream_player_node.finished.connect(_on_video_finished)
+	video_stream_player_node.play()
